ab5861a7fd7b2151baec6c14872336370a7160c9
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-05-08 14:52:01 +0200

    ⚙️ config (tailwind) : colors and font value (from style-guide)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        "neutral-light-bg": "hsl(180, 52%, 96%)",
+        "neutral-light-filter": "hsl(180, 31%, 95%)",
+        "neutral-dark": "hsl(180, 8%, 52%)",
+        "neutral-very-dark": "hsl(180, 14%, 20%)",
+      },
+      fontFamily: {
+        "league-spartan": ["League Spartan", "sans-serif"],
+      },
+      fontSize: {
+        base: "15px",
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
