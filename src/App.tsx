5830e0b76d1d7550465821798f18732eafb10083
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-05-09 15:38:38 +0200

    🔨feat : add badge and conditional rendering  for card style

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,11 @@ function JobCard({ job }: JobCardProps) {
   const details = [job.postedAt, job.contract, job.location];
   return (
     <>
-      <div className="bg-white p-7 mb-3 rounded flex justify-between items-center shadow-lg">
+      <div
+        className={`bg-white p-7 mb-3 rounded flex justify-between items-center shadow-lg ${
+          job.featured && "border-l-4 border-l-primary"
+        }`}
+      >
         <div className="flex items-center">
           <img
             src={logo}
@@ -50,16 +54,19 @@ function JobCard({ job }: JobCardProps) {
             className="w-full size-fit max-w-20 mr-5"
           />
           <div className="flex flex-col gap-1">
-            <p className="font-bold text-primary text-base">{job.company}</p>
-            <p className="font-bold">{job.position}</p>
-            <p className="">
-              {details.map((dt) => (
+            <p className="font-bold text-primary text-base flex gap-3">
+              {job.company}
+              {job.new && <Badge isNew={job.new} />}
+              {job.featured && <Badge featured={job.featured} />}
+            </p>
+            <p className="font-bold text-lg">{job.position}</p>
+            <div>
+              {details.map((detail) => (
                 <span className="text-neutral-dark font-medium mr-3">
-                  {" "}
-                  {dt}
+                  {detail}
                 </span>
               ))}
-            </p>
+            </div>
           </div>
         </div>
         <div>
@@ -80,5 +87,24 @@ function getImgUrl(name: string) {
     "https://raw.githubusercontent.com/damirdine/job-list-filter/main/src/"
   ).href;
 }
-
+type BadgeProps = {
+  isNew?: boolean;
+  featured?: boolean;
+};
+function Badge({ isNew, featured }: BadgeProps) {
+  if (isNew) {
+    return (
+      <p className="bg-primary text-white rounded-full text-sm me-2 px-1.5 py-1 pb-0 ">
+        NEW!
+      </p>
+    );
+  }
+  if (featured) {
+    return (
+      <span className="bg-neutral-very-dark text-white rounded-full text-sm me-2 px-1.5 py-1 pb-0">
+        FEATURED
+      </span>
+    );
+  }
+}
 export default App;
