b48d13ebf2ba351c698d9c18a90f2639641100a1
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-05-10 18:50:04 +0200

    🔨feat : mobile layout for reponsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import data from "./../data.json";
 
 import heroImg from "./assets/images/bg-header-desktop.svg";
+// import heroMobileImg from "./assets/images/bg-header-mobile.svg";
 
 type JobData = {
   id: number;
@@ -22,7 +23,12 @@ const jobs: JobData[] = data;
 function App() {
   return (
     <>
-      <img src={heroImg} alt="" className="bg-primary w-full" />
+      <header
+        className={`bg-[url('${getImgUrl(
+          "./assets/images/bg-header-desktop.svg"
+        )}')]`}
+      ></header>
+      <img src={heroImg} alt="" className="bg-primary w-full hidden md:block" />
       <main className="flex flex-col m-14 gap-2">
         {jobs.map((job) => (
           <JobCard key={job.id} job={job} />
@@ -43,35 +49,34 @@ function JobCard({ job }: JobCardProps) {
   return (
     <>
       <div
-        className={`bg-white p-7 mb-3 rounded flex justify-between items-center shadow-lg ${
+        className={`bg-white p-5 rounded flex flex-col justify-between md:items-center mb-10 gap-4 shadow-lg ${
           job.featured && "border-l-4 border-l-primary"
-        }`}
+        } md:bg-red-500 relative`}
       >
-        <div className="flex items-center">
-          <img
-            src={logo}
-            alt={job.company}
-            className="w-full size-fit max-w-20 mr-5"
-          />
-          <div className="flex flex-col gap-1">
-            <p className="font-bold text-primary text-base flex gap-3">
-              {job.company}
-              {job.new && <Badge isNew={job.new} />}
-              {job.featured && <Badge featured={job.featured} />}
-            </p>
-            <p className="font-bold text-lg">{job.position}</p>
-            <div>
-              {details.map((detail) => (
-                <span className="text-neutral-dark font-medium mr-3">
-                  {detail}
-                </span>
-              ))}
-            </div>
+        <img
+          src={logo}
+          alt={job.company}
+          className="w-full size-fit max-w-12 md:max-w-16 absolute -top-6 "
+        />
+        <div className="flex flex-col gap-1 mt-4 md:mt-0">
+          <p className="font-bold text-primary text-base flex gap-3">
+            {job.company}
+            <Badges isNew={job.new} featured={job.featured} />
+            {/* {job.new || job.featured && } */}
+          </p>
+          <p className="font-bold text-lg">{job.position}</p>
+          <div>
+            {details.map((detail) => (
+              <span className="text-neutral-dark font-medium mr-3">
+                {detail}
+              </span>
+            ))}
           </div>
         </div>
-        <div>
+        <hr className="md:hidden" />
+        <div className="flex flex-wrap gap-3">
           {tags.map((tag) => (
-            <span className="bg-neutral-light-bg text-primary p-2 font-bold ml-2 rounded">
+            <span className="bg-neutral-light-bg text-primary p-2 font-bold  rounded">
               {tag}
             </span>
           ))}
@@ -91,20 +96,21 @@ type BadgeProps = {
   isNew?: boolean;
   featured?: boolean;
 };
-function Badge({ isNew, featured }: BadgeProps) {
-  if (isNew) {
-    return (
-      <p className="bg-primary text-white rounded-full text-sm me-2 px-1.5 py-1 pb-0 ">
-        NEW!
-      </p>
-    );
-  }
-  if (featured) {
-    return (
-      <span className="bg-neutral-very-dark text-white rounded-full text-sm me-2 px-1.5 py-1 pb-0">
-        FEATURED
-      </span>
-    );
-  }
+function Badges({ isNew, featured }: BadgeProps) {
+  return (
+    <div className="flex justify-between">
+      {isNew && (
+        <span className="bg-primary text-white rounded-full text-sm px-2 py-1 pb-0 mx-2 ">
+          NEW!
+        </span>
+      )}
+      {featured && (
+        <span className="bg-neutral-very-dark text-white rounded-full text-sm px-1.5 py-1 pb-0">
+          FEATURED
+        </span>
+      )}
+    </div>
+  );
 }
+
 export default App;
