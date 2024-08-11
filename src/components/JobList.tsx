f6b9218ee160568c56df71ef4a6d31b4df8f9aec
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-08-11 17:54:25 +0200

    🚑 fix: filter component UI behavior and positioning

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -5,7 +5,7 @@ import { JobCard } from "./JobCard";
 export function JobList() {
   const { jobs } = useJobContext();
   return (
-    <main className="flex flex-col mx-7 my-12 md:mx-14 gap-2">
+    <main className="flex flex-col mx-7 my-12 md:mx-44 gap-2">
       {jobs.map((job) => (
         <JobCard key={job.id} job={job} />
       ))}
